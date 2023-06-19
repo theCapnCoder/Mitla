8033e7fb1565dfb523879a5b9ad9fe4ba76d3e0c
PavelM <myagkiypavel@gmail.com>
2023-06-19 18:57:59 +0300

    feature: containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import Container from "@mui/material/Container";
+import Header from "./containers/Header";
+import Main from "./containers/Main";
+import Discount from "./containers/Discount";
+import Explanation from "./containers/Explanation";
+import Service from "./containers/Service";
+import ExtraService from "./containers/ExtraService";
+import Questions from "./containers/Questions";
+import Support from "./containers/Support";
+import Partners from "./containers/Partners";
+import Branches from "./containers/Branches";
+import News from "./containers/News";
+import Order from "./containers/Order";
+import Footer from "./containers/Footer";
 
 function App() {
   return (
-    <Container maxWidth="xl" sx={{border: 2, borderColor: 'primary.main'}}>
-      <div>
-        <h2>Mitla</h2>
-      </div>
+    <Container maxWidth="xl" sx={{ border: 2, borderColor: "primary.main" }}>
+      <Header />
+      <Main />
+      <Discount />
+      <Explanation />
+      <Service />
+      <ExtraService />
+      <Questions />
+      <Support />
+      <Partners />
+      <Branches />
+      <News />
+      <Order />
+      <Footer />
     </Container>
   );
 }
