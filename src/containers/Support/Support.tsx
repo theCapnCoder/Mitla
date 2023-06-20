1ed6d8281cc138d58591bd2600df6da1aeb2d688
PavelM <myagkiypavel@gmail.com>
2023-06-20 18:03:44 +0300

    feature: Support

--- src/containers/Support/Support.tsx
+++ src/containers/Support/Support.tsx
@@ -1,10 +1,26 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, IconButton, Stack, Typography } from "@mui/material";
+import { Messenger } from "../../components/Icons/Messenger";
+import { Telegram } from "../../components/Icons/Telegram";
+import { Whatsapp } from "../../components/Icons/Whatsapp";
 
 export const Support = () => {
   return (
-    <div>
-      <Typography variant="h3">Служба поддержки 24/7</Typography>
-    </div>
-  )
-}
+    <Box sx={{ mt: "83px", mb: "150px" }}>
+      <Typography variant="h3" sx={{ mb: "27px" }}>
+        Служба поддержки 24/7
+      </Typography>
+
+      <Stack direction="row" justifyContent="center" spacing={1.25}>
+        <Button variant="contained" color="secondary" startIcon={<Telegram />}>
+          Telegram
+        </Button>
+        <Button variant="contained" color="primary" startIcon={<Messenger />}>
+          Messenger
+        </Button>
+        <Button variant="contained" color="success" startIcon={<Whatsapp />}>
+          WhatsApp
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
