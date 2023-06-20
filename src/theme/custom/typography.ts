50c17a61c66dd26ebd91e7b357f0583d8b8d538e
PavelM <myagkiypavel@gmail.com>
2023-06-20 10:41:22 +0300

    style: h1, h2

---
+++ src/theme/custom/typography.ts
@@ -0,0 +1,21 @@
+import { ThemeOptions } from "@mui/material/styles";
+
+export const typography: ThemeOptions = {
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+    h1: {
+      fontWeight: 700,
+      fontSize: 80,
+      lineHeight: '72px',
+      textAlign: 'center',
+      color: '#212121',
+    },
+    h2: {
+      fontWeight: 700,
+      fontSize: 48,
+      lineHeight: '60px',
+      textAlign: 'center',
+      color: '#212121',
+    }
+  },
+}
