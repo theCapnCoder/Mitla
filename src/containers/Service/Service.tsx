6a9ba8fbd95c2317eda3ea0e59161368ec760390
PavelM <myagkiypavel@gmail.com>
2023-06-21 12:18:10 +0300

    feature: add Service

--- src/containers/Service/Service.tsx
+++ src/containers/Service/Service.tsx
@@ -1,10 +1,24 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import { Card } from "./Card";
+import serviceCards from "./serviceCards";
 
 export const Service = () => {
   return (
-    <div>
-      <Typography variant="h2">Наши услуги</Typography>
-    </div>
-  )
-}
+    <Box sx={{ maxWidth: "1400px", margin: "0 auto", padding: "80px 0" }}>
+      <Typography variant="h2" sx={{ mb: "50px" }}>
+        Наши услуги
+      </Typography>
+      <Stack
+        direction="row"
+        gap={3.5}
+        maxWidth="calc((5 * 256px) + (30px*4))"
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {serviceCards.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
+      </Stack>
+    </Box>
+  );
+};
