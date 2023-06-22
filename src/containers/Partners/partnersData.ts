886b0504a47aff012f35a05cb86615b1c5f8e6f5
PavelM <myagkiypavel@gmail.com>
2023-06-22 17:47:23 +0300

    feature: add partnersData

---
+++ src/containers/Partners/partnersData.ts
@@ -0,0 +1,61 @@
+import image1 from '../../assets/img/partners/image 1.png';
+import image2 from '../../assets/img/partners/image 2.png';
+import image3 from '../../assets/img/partners/image 3.png';
+import image4 from '../../assets/img/partners/image 4.png';
+import image5 from '../../assets/img/partners/image 5.png';
+import image6 from '../../assets/img/partners/image 6.png';
+import image7 from '../../assets/img/partners/image 7.png';
+import image8 from '../../assets/img/partners/image 8.png';
+import image9 from '../../assets/img/partners/image 9.png';
+import image10 from '../../assets/img/partners/image 10.png';
+import image11 from '../../assets/img/partners/image 11.png';
+import image12 from '../../assets/img/partners/image 12.png';
+import image13 from '../../assets/img/partners/image 13.png';
+import image14 from '../../assets/img/partners/image 14.png';
+import image15 from '../../assets/img/partners/image 15.png';
+import image16 from '../../assets/img/partners/image 16.png';
+import image17 from '../../assets/img/partners/image 17.png';
+import image18 from '../../assets/img/partners/image 18.png';
+import image19 from '../../assets/img/partners/image 19.png';
+import image20 from '../../assets/img/partners/image 20.png';
+import image21 from '../../assets/img/partners/image 21.png';
+import image22 from '../../assets/img/partners/image 22.png';
+import image23 from '../../assets/img/partners/image 23.png';
+import image24 from '../../assets/img/partners/image 24.png';
+import image25 from '../../assets/img/partners/image 25.png';
+import image26 from '../../assets/img/partners/image 26.png';
+
+type Partner = {
+  img: string,
+}
+
+const partners: Partner[] = [
+  { img: image1 },
+  { img: image2 },
+  { img: image3 },
+  { img: image4 },
+  { img: image5 },
+  { img: image6 },
+  { img: image7 },
+  { img: image8 },
+  { img: image9 },
+  { img: image10 },
+  { img: image11 },
+  { img: image12 },
+  { img: image13 },
+  { img: image14 },
+  { img: image15 },
+  { img: image16 },
+  { img: image17 },
+  { img: image18 },
+  { img: image19 },
+  { img: image20 },
+  { img: image21 },
+  { img: image22 },
+  { img: image23 },
+  { img: image24 },
+  { img: image25 },
+  { img: image26 },
+]
+
+export default partners;
