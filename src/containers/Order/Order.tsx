f49c8f44d068b5eebccb91fae98a9732a90c3bff
PavelM <myagkiypavel@gmail.com>
2023-06-25 17:30:19 +0300

    chore: Order

--- src/containers/Order/Order.tsx
+++ src/containers/Order/Order.tsx
@@ -3,14 +3,6 @@ import React from "react";
 import { Counter } from "./Counter/Counter";
 
 export const Order = () => {
-  const res: Array<string | number> = ["one", "two", 12, 32];
-
-  console.log(
-    res.map((el) => {
-      return el;
-    })
-  );
-
   return (
     <Box width={"1400px"} m={"0 auto"}>
       <Typography variant="h2" mb={8.5}>
