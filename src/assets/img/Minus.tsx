f7237b0f33c31d6ff98a069d900d0d82376e503d
PavelM <myagkiypavel@gmail.com>
2023-06-25 11:01:52 +0300

    images: add minus and plus

---
+++ src/assets/img/Minus.tsx
@@ -0,0 +1,25 @@
+import { SvgIcon, SvgIconProps } from "@mui/material";
+
+export const Minus = (props: SvgIconProps) => {
+  return (
+    <SvgIcon
+      width="24"
+      height="2"
+      viewBox="0 0 24 2"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="&#208;&#156;&#208;&#184;&#208;&#189;&#209;&#131;&#209;&#129;">
+        <path
+          id="Vector 24"
+          d="M1 1H23"
+          stroke="#212121"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </SvgIcon>
+  );
+};
