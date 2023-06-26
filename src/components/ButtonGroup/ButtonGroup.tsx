d36d9d213024733105fb6faf2b1d4d5f77401875
PavelM <myagkiypavel@gmail.com>
2023-06-26 13:21:57 +0300

    chore: ButtonGroup

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,6 +1,5 @@
 import {
   Box,
-  Button,
   Stack,
   ToggleButton,
   ToggleButtonGroup,
@@ -17,6 +16,7 @@ export type ButtonGroupProps = {
 
 export type Props = {
   data: ButtonGroupProps[];
+  size?: "small" | "medium" | "large";
 };
 
 const DiscountTypography = styled(Typography)({
@@ -25,36 +25,9 @@ const DiscountTypography = styled(Typography)({
   lineHeight: "22px",
 });
 
-const TextTypography = styled(Typography)({
-  color: "#FFF",
-  fontSize: 18,
-  fontWeight: 700,
-  lineHeight: "22px",
-  textTransform: "none",
-});
-
-const CustomToggleButton = styled(ToggleButton)(({ theme }) => ({
-  padding: "40px",
-  backgroundColor: "aqua",
-  "&.Mui-selected": {
-    backgroundColor: "red",
-    color: "brown",
-    fontSize: 45,
-  },
-  "& .Mui-selected": {
-    backgroundColor: "red",
-    color: "brown",
-  },
-  "& Mui-selected": {
-    backgroundColor: "red",
-    color: "brown",
-  },
-}));
-
-export const ButtonGroup: FC<Props> = ({ data }) => {
+export const ButtonGroup: FC<Props> = ({ data, size }) => {
   const [alignment, setAlignment] = React.useState<string | null>("left");
   const [selected, setSelected] = React.useState(1);
-  console.log(selected);
 
   const handleAlignment = (
     event: React.MouseEvent<HTMLElement>,
@@ -64,77 +37,30 @@ export const ButtonGroup: FC<Props> = ({ data }) => {
   };
 
   return (
-    <>
-      <ToggleButtonGroup
-        value={alignment}
-        exclusive
-        onChange={handleAlignment}
-        aria-label="text alignment"
-      >
-        <ToggleButton value="left" aria-label="left aligned">
-          0
-        </ToggleButton>
-        <ToggleButton value="center" aria-label="centered">
-          1
-        </ToggleButton>
-        <ToggleButton value="right" aria-label="right aligned">
-          2
-        </ToggleButton>
-        <ToggleButton value="justify" aria-label="justified" disabled>
-          3
-        </ToggleButton>
-      </ToggleButtonGroup>
-
-      <ToggleButtonGroup
-        value={alignment}
-        exclusive
-        onChange={handleAlignment}
-        aria-label="text alignment"
-      >
-        <ToggleButton value="left" aria-label="left aligned">
-          Zero
-        </ToggleButton>
-        <ToggleButton value="center" aria-label="centered">
-          First
-        </ToggleButton>
-        <ToggleButton value="right" aria-label="right aligned">
-          Second
-        </ToggleButton>
-        <ToggleButton value="justify" aria-label="justified" disabled>
-          Third
-        </ToggleButton>
-      </ToggleButtonGroup>
-
-      {/* <ToggleButtonGroup
-        value={alignment}
-        exclusive
-        onChange={handleAlignment}
-        aria-label="text alignment"
-      >
-        {buttonData.map(({ discount, text, id }, index) => (
-          <ToggleButton
-            sx={{
-              // backgroundColor: selected === id ? "#F25E57" : "#FAFAFA",
-              backgroundColor: selected === id ? "red" : "blue",
-              // backgroundColor: 'yellow',
-              "&.Mui-selected": {
-                backgroundColor: "purple",
-              },
-            }}
-            onClick={(e, value) => setSelected(value)}
-            selected={selected === id}
-            key={index}
-            value={id}
-            aria-label="left aligned"
-          >
+    <ToggleButtonGroup
+      value={alignment}
+      exclusive
+      size={size}
+      onChange={handleAlignment}
+      aria-label="text alignment"
+      fullWidth
+    >
+      {data.map(({ text, discount, id }, index) => (
+        <ToggleButton
+          key={index}
+          selected={selected === id}
+          onClick={() => setSelected(id)}
+          value={id}
+          sx={{ flexGrow: 1 }}
+        >
+          {discount && (
             <Stack
               direction="row"
               columnGap="18px"
               alignItems={"center"}
               sx={{
-                backgroundColor: selected === id ? "#F25E57" : "#FAFAFA",
                 borderRadius: 1.5,
-                height: "100%",
+                mr: "18px",
               }}
             >
               <Box
@@ -148,15 +74,11 @@ export const ButtonGroup: FC<Props> = ({ data }) => {
               >
                 <DiscountTypography>{discount}</DiscountTypography>
               </Box>
-              <TextTypography
-                sx={{ color: selected === id ? "#FFF" : "#212121" }}
-              >
-                {text}
-              </TextTypography>
             </Stack>
-          </ToggleButton>
-        ))}
-      </ToggleButtonGroup> */}
-    </>
+          )}
+          {text}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   );
 };
