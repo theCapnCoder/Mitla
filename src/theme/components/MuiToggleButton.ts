44c276d7449858942e7822cc6b7040a8de58b0fc
PavelM <myagkiypavel@gmail.com>
2023-06-26 13:19:14 +0300

    style: fix hover ToggleButton

--- src/theme/components/MuiToggleButton.ts
+++ src/theme/components/MuiToggleButton.ts
@@ -17,13 +17,20 @@ export const MuiToggleButton: Props = {
         fontSize: 18,
         borderRadius: 6,
         fontWeight: 700,
+        color: '#212121',
         lineHeight: "22px",
         '&.Mui-selected': {
           backgroundColor: '#F25E57',
           borderRadius: 6,
           color: '#FFF',
+          "&:hover": {
+            backgroundColor: '#F25E57',
+          }
         },
       },
+      sizeSmall: {
+        padding: "18px 0 19px",
+      }
     },
   },
 }
