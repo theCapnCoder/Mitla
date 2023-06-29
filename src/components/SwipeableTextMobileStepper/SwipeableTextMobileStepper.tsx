5c30cd99b1e1a513906019d0d1600902e95c9864
PavelM <myagkiypavel@gmail.com>
2023-06-29 17:31:47 +0300

    chore: clear MobileStepper

--- src/components/SwipeableTextMobileStepper/SwipeableTextMobileStepper.tsx
+++ src/components/SwipeableTextMobileStepper/SwipeableTextMobileStepper.tsx
@@ -2,48 +2,20 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MobileStepper from "@mui/material/MobileStepper";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
 import { PrevArrow } from "../../assets/img/PrevArrow";
 import { NextArrow } from "../../assets/img/NextArrow";
 import { sliderData } from "./sliderData";
-import { Avatar, Stack } from "@mui/material";
 import { UserInfo } from "./UserInfo";
 
 const AutoPlaySwipeableViews = SwipeableViews;
 
-const images = [
-  {
-    label: "San Francisco – Oakland Bay Bridge, United States",
-    imgPath:
-      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Bird",
-    imgPath:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Bali, Indonesia",
-    imgPath:
-      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
-  },
-  {
-    label: "Goč, Serbia",
-    imgPath:
-      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-];
-
 export const SwipeableTextMobileStepper = () => {
   const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
+  const maxSteps = sliderData.length;
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -113,7 +85,7 @@ export const SwipeableTextMobileStepper = () => {
           margin: "0 auto",
           flexGrow: 1,
           backgroundColor: "transparent",
-          height: "227px",
+          height: "300",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -130,39 +102,9 @@ export const SwipeableTextMobileStepper = () => {
               <div key={step.text}>
                 {Math.abs(activeStep - index) <= 2 ? (
                   <UserInfo {...step} />
-                ) : // <Box
-                //   component="img"
-                //   sx={{
-                //     height: 255,
-                //     display: "block",
-                //     maxWidth: 400,
-                //     overflow: "hidden",
-                //     width: "100%",
-                //   }}
-                //   src={step.imgPath}
-                //   alt={step.label}
-                // />
-                null}
-              </div>
-            ))}
-            {/* {images.map((step, index) => (
-              <div key={step.label}>
-                {Math.abs(activeStep - index) <= 2 ? (
-                  <Box
-                    component="img"
-                    sx={{
-                      height: 255,
-                      display: "block",
-                      maxWidth: 400,
-                      overflow: "hidden",
-                      width: "100%",
-                    }}
-                    src={step.imgPath}
-                    alt={step.label}
-                  />
                 ) : null}
               </div>
-            ))} */}
+            ))}
           </AutoPlaySwipeableViews>
         </Box>
       </Box>
