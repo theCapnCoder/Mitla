96b6dd754d046a656317384ffca01bb539139427
PavelM <myagkiypavel@gmail.com>
2023-06-29 18:45:21 +0300

    style: Main

--- src/containers/Main/ButtonsGroup/ButtonsGroup.tsx
+++ src/containers/Main/ButtonsGroup/ButtonsGroup.tsx
@@ -3,8 +3,8 @@ import { Button, Stack } from "@mui/material";
 export const ButtonsGroup = () => {
   return (
     <Stack direction={"row"} spacing={2.5} justifyContent={"center"} >
-      <Button variant="outlined" size="medium">Оставить свой отзыв  →</Button>
-      <Button variant="outlined" size="medium">Все отзывы  →</Button>
+      <Button variant="outlined" size="large">Оставить свой отзыв  →</Button>
+      <Button variant="outlined" size="large">Все отзывы  →</Button>
     </Stack>
   );
 };
--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,20 +1,39 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
 import { SwipeableTextMobileStepper } from "../../components/SwipeableTextMobileStepper";
 import { ButtonsGroup } from "./ButtonsGroup";
+import bg from "../../assets/img/bg.jpg";
+import Order from "../Order";
+import { Counter } from "../../components/Calculate/Counter/Counter";
+import { Calculate } from "../../components/Calculate";
+
+const BackgroundBox = styled(Box)({
+  height: "820px",
+  background: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #25B2B4 100%), url(${bg})`,
+});
 
 export const Main = () => {
   return (
-    <Box sx={{ pb: 16.88, backgroundColor: "#25B2B4" }}>
-      <Typography variant="h1">Закажи уборку квартиры</Typography>
-      <Typography variant="h4">
-        В услугу входит уборка комнаты, кухни, ванной комнаты и коридора
-      </Typography>
+    <Box>
+      <BackgroundBox pt={27}>
+        <Typography mb={4} variant="h1">
+          Закажи уборку квартиры
+        </Typography>
+        <Typography mb={7} variant="h4">
+          В услугу входит уборка комнаты, кухни, ванной комнаты и коридора
+        </Typography>
 
-      <Box mb={5}>
-        <SwipeableTextMobileStepper />
+        <Box width={"1400px"} m={"0 auto"}>
+          <Calculate />
+        </Box>
+      </BackgroundBox>
+
+      <Box sx={{ pb: '145px', backgroundColor: "#25B2B4" }}>
+        <Box mb={5}>
+          <SwipeableTextMobileStepper />
+        </Box>
+
+        <ButtonsGroup />
       </Box>
-
-      <ButtonsGroup />
     </Box>
   );
 };
