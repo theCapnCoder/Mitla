cdd70f61b506a1847c81e0cbd3152ea4e10e459a
PavelM <myagkiypavel@gmail.com>
2023-06-29 17:51:08 +0300

    chore: Main add ButtonGroup

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,15 +1,20 @@
 import { Box, Typography } from "@mui/material";
 import { SwipeableTextMobileStepper } from "../../components/SwipeableTextMobileStepper";
+import { ButtonsGroup } from "./ButtonsGroup";
 
 export const Main = () => {
   return (
-    <Box sx={{ backgroundColor: "#25B2B4" }}>
+    <Box sx={{ pb: 16.88, backgroundColor: "#25B2B4" }}>
       <Typography variant="h1">Закажи уборку квартиры</Typography>
       <Typography variant="h4">
         В услугу входит уборка комнаты, кухни, ванной комнаты и коридора
       </Typography>
 
-      <SwipeableTextMobileStepper />
+      <Box mb={5}>
+        <SwipeableTextMobileStepper />
+      </Box>
+
+      <ButtonsGroup />
     </Box>
   );
 };
