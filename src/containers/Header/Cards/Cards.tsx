8872aebf18c155d3bd4a560aa363304e5617c559
PavelM <myagkiypavel@gmail.com>
2023-06-30 19:33:21 +0300

    chore: Cards

--- src/containers/Header/Cards/Cards.tsx
+++ src/containers/Header/Cards/Cards.tsx
@@ -1,17 +1,22 @@
-import { Box, Paper, Stack, Typography } from "@mui/material";
-import React from "react";
+import { Box, Paper, Stack, Typography, styled } from "@mui/material";
 import { cards } from "./cardsData";
+
+const TextTypography = styled(Typography)({
+  fontSize: 13,
+  fontWeight: 500,
+  lineHeight: "18px",
+})
 
 export const Cards = () => {
   return (
-    <Stack direction={"row"}>
+    <Stack direction={"row"} maxWidth={"702px"}>
       {cards.map(({ title }) => (
         <Paper elevation={0}>
           <Stack key={title} alignItems={"center"}>
             <Box
               sx={{marginBottom: 1, width: "38px", height: "38px", backgroundColor: "#ECECEC" }}
             ></Box>
-            <Typography>{title}</Typography>
+            <TextTypography>{title}</TextTypography>
           </Stack>
         </Paper>
       ))}
