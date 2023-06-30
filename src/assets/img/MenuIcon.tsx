31452d92ffadc900dade749f8380599c0cb77b77
PavelM <myagkiypavel@gmail.com>
2023-06-30 16:05:05 +0300

    images: add menu icons

---
+++ src/assets/img/MenuIcon.tsx
@@ -0,0 +1,44 @@
+import { SvgIcon, SvgIconProps } from "@mui/material";
+
+export const MenuIcon = (props: SvgIconProps) => {
+  return (
+    <SvgIcon
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="&#208;&#156;&#208;&#181;&#208;&#189;&#209;&#142;">
+        <rect id="bg" width="60" height="60" rx="4" fill="#ECECEC" />
+        <g id="Menu">
+          <rect
+            id="Rectangle 3"
+            width="24.3243"
+            height="1.82561"
+            rx="0.912804"
+            transform="matrix(-1 0 0 1 45 18)"
+            fill="#212121"
+          />
+          <rect
+            id="Rectangle 5"
+            width="18.6486"
+            height="1.82561"
+            rx="0.912804"
+            transform="matrix(-1 0 0 1 45 39.9075)"
+            fill="#212121"
+          />
+          <rect
+            id="Rectangle 4"
+            width="30"
+            height="1.82561"
+            rx="0.912804"
+            transform="matrix(-1 0 0 1 45 28.9535)"
+            fill="#212121"
+          />
+        </g>
+      </g>
+    </SvgIcon>
+  );
+};
