086d40f4cee247c2ee612783e2a45f0c27766391
PavelM <myagkiypavel@gmail.com>
2023-07-01 21:07:23 +0300

    feature: add pixel perfect Data

---
+++ src/containers/Documentation/PixelPerfect/pixelPerfectData.ts
@@ -0,0 +1,33 @@
+import img1 from '../assets/images/picture1.jpg';
+import img2 from '../assets/images/picture2.jpg';
+import img3 from '../assets/images/picture3.jpg';
+import img4 from '../assets/images/picture4.jpg';
+import img5 from '../assets/images/picture5.jpg';
+import img6 from '../assets/images/picture6.jpg';
+import img7 from '../assets/images/picture7.jpg';
+import img8 from '../assets/images/picture8.jpg';
+import img9 from '../assets/images/picture9.jpg';
+import img10 from '../assets/images/picture10.jpg';
+import img11 from '../assets/images/picture11.jpg';
+import img12 from '../assets/images/picture12.jpg';
+import img13 from '../assets/images/picture13.jpg';
+
+export type Card = {
+  img: string,
+}
+
+export const pixelPerfectData: Card[] = [
+  { img: img1 },
+  { img: img2 },
+  { img: img3 },
+  { img: img4 },
+  { img: img5 },
+  { img: img6 },
+  { img: img7 },
+  { img: img8 },
+  { img: img9 },
+  { img: img10 },
+  { img: img11 },
+  { img: img12 },
+  { img: img13 },
+]
